7e5bb81232700b0e0d3c0a700f15ae6f06c0ed37
Kevin Lacker <lacker@gmail.com>
2018-05-09 18:15:29 -0700

    AvailableMana

--- game/player.go
+++ game/player.go
@@ -3,10 +3,20 @@ package game
 import ()
 
 type Player struct {
-	life  int
-	hand  []*Card
-	board []*Card
+	Life  int
+	Hand  []*Card
+	Board []*Card
 
 	// 0 = on the play, 1 = on the draw
-	index int
+	Index int
+}
+
+func (p *Player) AvailableMana() int {
+	answer := 0
+	for _, card := range p.Board {
+		if card.IsLand {
+			answer += 1
+		}
+	}
+	return answer
 }
