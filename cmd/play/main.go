b3412d649d083b156c09fa5a8bd40c1f0cffc9a9
Andrew Johnson <andrewjohnson@Andrews-iMac.local>
2018-05-10 21:37:32 -0700

    hand printing

--- cmd/play/main.go
+++ cmd/play/main.go
@@ -11,7 +11,7 @@ func main() {
 	var i int
 	for start := time.Now(); time.Since(start) < time.Second; {
 		fmt.Println("game started")
-		playOutGameRandomly()
+		playOutGameRandomly(i)
 		fmt.Println("game ended")
 	    i++
 	}
@@ -21,15 +21,24 @@ func main() {
 }
 
 
-func playOutGameRandomly() {
+func playOutGameRandomly(gameNumber int) {
 	game := game.NewGame()
 	fmt.Println("game created")
+	var i int
 	for {
 		// fmt.Printf("M")
 		actions := game.Actions()
 		randomAction := actions[rand.Int() % len(actions)]
 		game.TakeAction(randomAction)
 		if game.IsOver() { break }
+		i++
+		if i % 9999 == 0 {
+			fmt.Println(randomAction)
+		}
+		if i % 10000 == 0 {
+			fmt.Println("Game Number ", gameNumber, " Move # ", i)
+			game.Print()
+		}
 	}
 
 }
--- game/card.go
+++ game/card.go
@@ -77,6 +77,81 @@ func (c *Card) Print() {
 	printCardBorder(cardWidth)
 }
 
+
+func (c *Card) AsciiImage() {
+	cardWidth := 11
+	cardHeight := 5
+	imageGrid := [cardHeight][cardWidth]int
+	for y := 0; y < cardHeight; y++ {
+		for x := 0; x < cardWidth; x++ {
+				if x == 0 or x == cardWidth - 1:
+					imageGrid[-1].append('|')
+				elif y == 0 or y == cardHeight - 1:
+					imageGrid[-1].append('-')
+				else:
+					imageGrid[-1].append(' ')
+		}
+	}
+
+	initialIndex := 2
+
+	if showBack:
+		middleX := cardWidth / 2
+		middleY := cardHeight / 2
+
+		noon := (middleX, middleY - 1, '*')
+		two := (middleX + 2, middleY, '*')
+		ten := (middleX - 2, middleY, '*')
+		seven := (middleX - 1, middleY + 1, '*')
+		four := (middleX + 1, middleY + 1, '*')
+
+		points := [noon, two, four, seven, ten]
+		for _, p := range points {
+			imageGrid[p[1]][p[0]] = p[2]
+		}
+	
+	if !showBack {
+		ccRow = 1
+		ccString = fmt.Sprintf("%v", c.ManaCost)
+		for x in range(initialIndex, len(ccString) + initialIndex):
+			imageGrid[ccRow][x] = ccString[x-initialIndex]
+	}	
+
+	if !showBack:
+		nameRow = 2
+		words := fmt.Sprintf("%v", c.Name)
+		for _, word := range words {
+			wordWidth = min(3, len(word))
+			if len(words) == 1:
+				wordWidth = min(len(word), cardWidth - 4)
+			for x in range(initialIndex, wordWidth + initialIndex):
+				imageGrid[nameRow][x] = word[x-initialIndex]
+			initialIndex += wordWidth + 1
+			if initialIndex >= cols - wordWidth - 1:
+				break
+
+	if not showBack:
+		if c.IsCreature {
+			initialIndex = 2
+			statsRow = 3
+			statsString = fmt.Sprintf("%v/%v", c.Power, c.Toughnesss)
+			for x in range(initialIndex, len(statsString) + initialIndex):
+				image_grid[statsRow][x] = statsString[x-initialIndex]
+		}
+
+	if not show_back:
+		if c.IsTapped {
+		if Card.tapped(card_state):
+			tapped_row = 0
+			initial_index = 0
+			tapped_string = "TAPPED"
+			for x in range(initial_index, len(tapped_string) + initial_index):
+				image_grid[tapped_row][x] = tapped_string[x-initial_index]
+
+	return image_grid
+}
+
+
 func printCardBorder(cardWidth int) {
 	for i := 0; i < cardWidth; i++ {
 		fmt.Printf("-")
