eb74ba936a3a42414ee45bda6006f4f2f6b0026b
Kevin Lacker <lacker@gmail.com>
2018-05-14 21:31:28 -0700

    replace Priority with PriorityId

--- game/game.go
+++ game/game.go
@@ -6,6 +6,25 @@ import (
 
 const GAME_WIDTH = 100
 
+type PlayerId int
+
+const (
+	OnThePlay PlayerId = iota
+	OnTheDraw
+)
+
+func (id PlayerId) OpponentId() PlayerId {
+	switch id {
+	case OnThePlay:
+		return OnTheDraw
+	case OnTheDraw:
+		return OnThePlay
+	default:
+		panic("bad id")
+	}
+	panic("bad logic")
+}
+
 type Game struct {
 	// Players are sometimes referred to by index, 0 or 1.
 	// Player 0 is the player who plays first.
@@ -16,8 +35,8 @@ type Game struct {
 	// In general, it is player (turn % 2)'s turn.
 	Turn int
 
-	// Which player has priority
-	Priority *Player
+	// The id of the player with priority
+	PriorityId PlayerId
 }
 
 type Phase int
@@ -40,10 +59,10 @@ func NewGame(deckToPlay *Deck, deckToDraw *Deck) *Game {
 		NewPlayer(deckToDraw),
 	}
 	g := &Game{
-		Players:  players,
-		Phase:    Main1,
-		Turn:     0,
-		Priority: players[0],
+		Players:    players,
+		Phase:      Main1,
+		Turn:       0,
+		PriorityId: OnThePlay,
 	}
 
 	players[0].Opponent = players[1]
@@ -55,40 +74,57 @@ func NewGame(deckToPlay *Deck, deckToDraw *Deck) *Game {
 	return g
 }
 
+// Player(id) returns the player with the provided id.
+func (g *Game) Player(id PlayerId) *Player {
+	return g.Players[id]
+}
+
+func (g *Game) Priority() *Player {
+	return g.Players[g.PriorityId]
+}
+
 func (g *Game) Actions(forHuman bool) []*Action {
 	actions := []*Action{}
 	switch g.Phase {
 	case Main1:
-		actions = append(actions, g.Priority.PlayActions(true, forHuman)...)
+		actions = append(actions, g.Priority().PlayActions(true, forHuman)...)
 		if g.canAttack() {
 			actions = append(actions, &Action{Type: DeclareAttack})
 		}
-		return append(actions, g.Priority.ManaActions()...)
+		return append(actions, g.Priority().ManaActions()...)
 	case Main2:
-		actions = g.Priority.PlayActions(true, forHuman)
-		return append(actions, g.Priority.ManaActions()...)
+		actions = g.Priority().PlayActions(true, forHuman)
+		return append(actions, g.Priority().ManaActions()...)
 	case DeclareAttackers:
-		return append(g.Priority.AttackActions(), g.Priority.PassAction())
+		return append(g.Priority().AttackActions(), g.Priority().PassAction())
 	case DeclareBlockers:
-		return append(g.Priority.BlockActions(), g.Priority.PassAction())
+		return append(g.Priority().BlockActions(), g.Priority().PassAction())
 	default:
 		panic("unhandled phase")
 	}
 }
 
+func (g *Game) AttackerId() PlayerId {
+	return PlayerId(g.Turn % 2)
+}
+
 func (g *Game) Attacker() *Player {
-	return g.Players[g.Turn%2]
+	return g.Players[g.AttackerId()]
+}
+
+func (g *Game) DefenderId() PlayerId {
+	return g.AttackerId().OpponentId()
 }
 
 func (g *Game) Defender() *Player {
-	return g.Attacker().Opponent
+	return g.Players[g.DefenderId()]
 }
 
 func (g *Game) canAttack() bool {
 	if g.Phase != Main1 {
 		return false
 	}
-	for _, card := range g.Priority.Board {
+	for _, card := range g.Priority().Board {
 		if card.CanAttack(g) {
 			return true
 		}
@@ -135,9 +171,9 @@ func (g *Game) HandleCombatDamage() {
 }
 
 func (g *Game) Creatures() []*Card {
-	answer := g.Priority.Creatures()
-	for _, card := range g.Priority.Opponent.Creatures() {
-		answer = append(answer, card)
+	answer := []*Card{}
+	for _, player := range g.Players {
+		answer = append(answer, player.Creatures()...)
 	}
 	return answer
 }
@@ -148,13 +184,13 @@ func (g *Game) nextPhase() {
 		g.Phase = DeclareAttackers
 	case DeclareAttackers:
 		g.Phase = DeclareBlockers
-		g.Priority = g.Defender()
+		g.PriorityId = g.DefenderId()
 	case DeclareBlockers:
 		g.HandleCombatDamage()
 		g.Attacker().EndCombat()
 		g.Defender().EndCombat()
 		g.Phase = Main2
-		g.Priority = g.Attacker()
+		g.PriorityId = g.AttackerId()
 	case Main2:
 		// End the turn
 		for _, p := range g.Players {
@@ -162,9 +198,9 @@ func (g *Game) nextPhase() {
 		}
 		g.Phase = Main1
 		g.Turn++
-		g.Priority.Untap()
-		g.Priority = g.Priority.Opponent
-		g.Priority.Draw()
+		g.Priority().Untap()
+		g.PriorityId = g.PriorityId.OpponentId()
+		g.Priority().Draw()
 	}
 }
 
@@ -192,7 +228,7 @@ func (g *Game) TakeAction(action *Action) {
 		fallthrough
 	case Main2:
 		if action.Type == Play {
-			g.Priority.Play(action)
+			g.Priority().Play(action)
 		} else {
 			panic("expected a play, declare attack, or pass during main phase")
 		}
@@ -217,7 +253,7 @@ func (g *Game) TakeAction(action *Action) {
 }
 
 func (g *Game) IsOver() bool {
-	return g.Priority.Lost() || g.Priority.Opponent.Lost()
+	return g.Attacker().Lost() || g.Defender().Lost()
 }
 
 func (g *Game) Print() {
@@ -252,7 +288,7 @@ func printMiddleLine(gameWidth int) {
 // 0 or 1 depending on who has priority
 func (g *Game) PriorityIndex() int {
 	for i, player := range g.Players {
-		if player == g.Priority {
+		if player == g.Priority() {
 			return i
 		}
 	}
@@ -282,7 +318,7 @@ func (g *Game) passTurn() {
 
 // playLand plays the first land it sees in the hand
 func (g *Game) playLand() {
-	for _, a := range g.Priority.PlayActions(true, false) {
+	for _, a := range g.Priority().PlayActions(true, false) {
 		if a.Card != nil && a.Card.IsLand {
 			g.TakeAction(a)
 			return
@@ -294,7 +330,7 @@ func (g *Game) playLand() {
 
 // playCreature plays the first creature it sees in the hand
 func (g *Game) playCreature() {
-	for _, a := range g.Priority.PlayActions(true, false) {
+	for _, a := range g.Priority().PlayActions(true, false) {
 		if a.Card != nil && a.Card.IsCreature {
 			g.TakeAction(a)
 			return
@@ -306,7 +342,7 @@ func (g *Game) playCreature() {
 
 // playInstant plays the first instant it sees in the hand
 func (g *Game) playInstant() {
-	for _, a := range g.Priority.PlayActions(true, false) {
+	for _, a := range g.Priority().PlayActions(true, false) {
 		if a.Card != nil && a.Card.IsInstant && a.Type == Play {
 			g.TakeAction(a)
 			return
@@ -318,7 +354,7 @@ func (g *Game) playInstant() {
 
 // playKickedInstant kicks the first kickable instant it sees in the hand
 func (g *Game) playKickedInstant() {
-	for _, a := range g.Priority.PlayActions(true, false) {
+	for _, a := range g.Priority().PlayActions(true, false) {
 		if a.Card != nil && a.Card.IsInstant && a.WithKicker {
 			g.TakeAction(a)
 			return
@@ -331,9 +367,9 @@ func (g *Game) playKickedInstant() {
 // attackWithEveryone passes priority when it's done attacking
 func (g *Game) attackWithEveryone() {
 	for {
-		actions := g.Priority.AttackActions()
+		actions := g.Priority().AttackActions()
 		if len(actions) == 0 {
-			g.TakeAction(g.Priority.PassAction())
+			g.TakeAction(g.Priority().PassAction())
 			return
 		}
 		g.TakeAction(actions[0])
