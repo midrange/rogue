570d11ac45b23345e29d4b34a617f630ae4c41ea
Andrew Johnson <andrewljohnson@gmail.com>
2018-06-09 15:24:24 -0700

    make attack bot smarter

--- game/attack_bot.go
+++ game/attack_bot.go
@@ -22,8 +22,11 @@ func (ab *AttackBot) Action(g *Game) *Action {
 
 	for _, a := range actions {
 		if a.Type == Play {
+			if a.isOpponentBuff() {
+				continue
+			}
 			if bestAction.Type == Play {
-				if a.Card.CastingCost != nil && a.Card.CastingCost.Colorless > bestAction.Card.CastingCost.Colorless {
+				if a.Card.CastingCost != nil && bestAction.Card.CastingCost != nil && a.Card.CastingCost.Colorless > bestAction.Card.CastingCost.Colorless {
 					bestAction = a
 				}
 			} else {
@@ -48,5 +51,25 @@ func (ab *AttackBot) Action(g *Game) *Action {
 			bestAction = a
 		}
 	}
+
+	for _, a := range actions {
+		if !bestAction.isOpponentBuff() {
+			break
+		}
+		bestAction = a
+	}
+
+	for _, a := range actions {
+		if bestAction.Source == nil || bestAction.Source.Card.Name != QuirionRanger {
+			break
+		}
+		bestAction = a
+	}
 	return bestAction
+}
+
+func (a *Action) isOpponentBuff() bool {
+	c := a.Card
+	return a.Target != nil && a.Target.Owner != a.Target.Owner.game.Priority() && (c.Name == Rancor || c.Name == VinesOfVastwood ||
+		c.Name == MutagenicGrowth || c.Name == HungerOfTheHowlpack)
 }
